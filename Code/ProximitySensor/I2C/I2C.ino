ed009b9a4fa951f334a938dbaa60008e2007cb0d
Felix de Neve <pixelneenjah@gmail.com>
2018-04-24 20:35:08 +0100

    I2C address change

--- Code/ProximitySensor/I2C/I2C.ino
+++ Code/ProximitySensor/I2C/I2C.ino
@@ -4,12 +4,16 @@
  * MTCH112 default I2C address is 0xE6 this needs to be right shifted for the arduino library
 */
 #include <Wire.h>
-#define ADDRESS 115
+#define DEFAULT_ADDRESS 115
+#define ADDRESS 2
 void setup() {
   // put your setup code here, to run once:
   Wire.begin();
   Serial.begin(9600); 
   Serial.println("Initialising MTCH112...");
+  WriteAddress();
+  Serial.println("Address: ");
+  ReadRegister(0x0B);
   WriteRegister(0x07,150);
   Serial.println("Prox threshold: ");
   ReadRegister(0x07);
@@ -23,13 +27,13 @@ void setup() {
 void loop() {
   int distance = 0;
   //Send I^2C frame to device
-  Wire.beginTransmission(115);
+  Wire.beginTransmission(ADDRESS);
   //This indicates which register to read
   Wire.write(0x80);
   Wire.endTransmission();
   delay(50);
   //Read back the data sent by the MTCH112
-  Wire.requestFrom(115,2,1);
+  Wire.requestFrom(ADDRESS,2,1);
   if (2 <= Wire.available()) { // Wait till 2 bytes are ready.
     //Receive the two bytes and shift them accordingly
     distance = Wire.read(); 
@@ -88,4 +92,20 @@ void ReadRegister(int reg){
   //Wait before reading or writing again
   delay(50);
 }
-
+void WriteAddress(){
+  //Open writing I2C to device with ADDRESS
+  Wire.beginTransmission(DEFAULT_ADDRESS);
+  //0x55 and 0xAA Sent as write protection
+  Wire.write(0x55);
+  Wire.write(0xAA);
+  //Queue the location of the register
+  Wire.write(0x0B);
+  //Queue the new value for the register
+  Wire.write(ADDRESS<<1);
+  //Queue a checksum (Xor of all bytes sent except address)
+  Wire.write(0x55^0xAA^0x0B^(ADDRESS<<1));
+  //Send all queued bytes
+  Wire.endTransmission();
+  //Wait before reading or writing again
+  delay(50);
+}
